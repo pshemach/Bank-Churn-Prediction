d5d1e0a96fda8ede2d1702a74023deaeb5467499
Pasindu <pasindusandakan@gmail.com>
2024-06-16 20:06:28 +0900

    template added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bankChurn",
+    version="0.0.0",
+    author="Pasindu",
+    author_email="pasindusandakan@gmail.com",
+    packages=find_packages()
+)
